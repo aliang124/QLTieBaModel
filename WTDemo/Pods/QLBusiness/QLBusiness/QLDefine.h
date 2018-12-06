9107f2b16b40a461cc18dbf8b66211b289261578
计恩良 <454309007@qq.com>
2018-12-06 17:20:42 +0800

    1
    1

--- WTDemo/Pods/QLBusiness/QLBusiness/QLDefine.h
+++ WTDemo/Pods/QLBusiness/QLBusiness/QLDefine.h
@@ -10,5 +10,16 @@
 #define QLBusiness_QLDefine_h
 //导航栏的黄色
 #define QL_NavBar_BgColor_Yellow WTColorHex(0xF8DC3D)
+//导航栏标题颜色黑色
+#define QL_NavBar_TitleColor_Black WTColorHex(0x232A34)
+#define QL_UserName_TitleColor_Black WTColorHex(0x36342A)
+//描述字体颜色灰色
+#define QL_DescColor_Gray WTColorHex(0x63625C)
+
+//多页面滑动的下面游标颜色
+#define QL_NavBar_CursorColor_Black WTColorHex(0x4D4207)
+//线条颜色
+#define QL_TableView_LineColor WTColorHex(0xDDE5EA)
+#define QL_Border_LineColor WTColorHex(0xDBDBDB)
 
 #endif
