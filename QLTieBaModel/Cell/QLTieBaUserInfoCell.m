d0a99c3e058077fadad3499ac697999c5bd42807
计恩良 <454309007@qq.com>
2019-01-01 02:22:18 +0800

    1
    1

--- QLTieBaModel/Cell/QLTieBaUserInfoCell.m
+++ QLTieBaModel/Cell/QLTieBaUserInfoCell.m
@@ -22,7 +22,13 @@
 
 @interface QLTieBaUserInfoCell()
 {
-    UILabel *titleLab;
+    UIImageView *iconImg;
+    UILabel *userNameLab;
+    
+    UILabel *liuYanCountLab;
+    UIImageView *liuYanIcon;
+    UILabel *liulanCountLab;
+    UIImageView *liulanIcon;
 }
 @end
 
@@ -31,11 +37,38 @@
 - (void)cellDidLoad
 {
     [super cellDidLoad];
-    titleLab = [[UILabel alloc] initWithFrame:CGRectMake(16, 0, WTScreenWidth-16-40, 5)];
-    titleLab.font = WTFontBoldSys(16);
-    titleLab.numberOfLines = 0;
-    titleLab.textColor = QL_UserName_TitleColor_Black;
-    [self.contentView addSubview:titleLab];
+    self.backgroundColor = [UIColor whiteColor];
+    
+    iconImg = [[UIImageView alloc] initWithFrame:CGRectMake(16, 15, 20, 20)];
+    iconImg.layer.cornerRadius = 10;
+    iconImg.layer.masksToBounds = YES;
+    iconImg.backgroundColor = [UIColor redColor];
+    [self.contentView addSubview:iconImg];
+    
+    userNameLab = [[UILabel alloc] initWithFrame:CGRectMake(iconImg.right+10, 19, 0, 12)];
+    userNameLab.font = WTFontSys(12);
+    userNameLab.numberOfLines = 0;
+    userNameLab.textColor = QL_UserName_TitleColor_Black;
+    [self.contentView addSubview:userNameLab];
+
+    //留言数
+    liuYanCountLab = [[UILabel alloc] initWithFrame:CGRectMake(WTScreenWidth-16, 21, 9, 7)];
+    liuYanCountLab.font = WTFontSys(10);
+    liuYanCountLab.textColor = QL_DescColor_Gray;
+    [self.contentView addSubview:liuYanCountLab];
+    
+    liuYanIcon = [[UIImageView alloc] initWithFrame:CGRectMake(0, 20, 10, 10)];
+    [liuYanIcon setImage:[UIImage imageNamed:@"liuyan"]];
+    [self.contentView addSubview:liuYanIcon];
+    //浏览数
+    liulanCountLab = [[UILabel alloc] initWithFrame:CGRectMake(WTScreenWidth-12, 21, 9, 7)];
+    liulanCountLab.font = WTFontSys(10);
+    liulanCountLab.textColor = QL_DescColor_Gray;
+    [self.contentView addSubview:liulanCountLab];
+    
+    liulanIcon = [[UIImageView alloc] initWithFrame:CGRectMake(0, 20, 10, 10)];
+    [liulanIcon setImage:[UIImage imageNamed:@"liulan"]];
+    [self.contentView addSubview:liulanIcon];
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated{}
@@ -44,8 +77,21 @@
 - (void)cellWillAppear
 {
     [super cellWillAppear];
-    self.backgroundColor = [UIColor whiteColor];
-    titleLab.text = [WTUtil strRelay:self.item.titleText];
+    userNameLab.text = @"用户A";
+    [userNameLab sizeToFit];
+    userNameLab.top = (50-userNameLab.height)/2;
+    //留言数
+    liuYanCountLab.text = @"1224";
+    [liuYanCountLab sizeToFit];
+    liuYanCountLab.top = (50-liuYanCountLab.height)/2;
+    liuYanCountLab.left = WTScreenWidth-16-liuYanCountLab.width;
+    liuYanIcon.left = liuYanCountLab.left-5-10;
+    //浏览数
+    liulanCountLab.text = @"8180";
+    [liulanCountLab sizeToFit];
+    liulanCountLab.top = (50-liulanCountLab.height)/2;
+    liulanCountLab.left = liuYanIcon.left-16-liulanCountLab.width;
+    liulanIcon.left = liulanCountLab.left-5-10;
 }
 
 - (void)layoutSubviews
